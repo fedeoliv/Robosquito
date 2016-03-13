1977cc2ab5c3f2750f59645883cccc3bd7ccfa1c
Fernando de Oliveira <t-fedeol@microsoft.com>
2016-03-13 17:58:16 -0300

    Time range validation

--- tests/tests.ino
+++ tests/tests.ino
@@ -1,13 +1,28 @@
 #include <Time.h>
 #include <TimeAlarms.h>
 
+const static int MIN_HOUR_LIMIT = 11;           // Minimum hour limit for robot movements
+const static int MIN_MINUTE_LIMIT = 20;         // Minimum minute limit for robot movements
+const static int MIN_SECOND_LIMIT = 0;          // Minimum second limit for robot movements
+
+const static int MAX_HOUR_LIMIT = 11;           // Maximum hour limit for robot movements
+const static int MAX_MINUTE_LIMIT = 21;         // Maximum minute limit for robot movements
+const static int MAX_SECOND_LIMIT = 0;          // Maximum second limit for robot movements
+
+const static float minHour = MIN_HOUR_LIMIT + (MIN_MINUTE_LIMIT / 60.0) + (MIN_SECOND_LIMIT / 3600.0);
+const static float maxHour = MAX_HOUR_LIMIT + (MAX_MINUTE_LIMIT / 60.0) + (MAX_SECOND_LIMIT / 3600.0);
+
 void setup()
 {
   Serial.begin(9600);
-  setTime(11, 19, 0, 3, 13, 16);                  // 11:19:00 Março 13 2016
+  setDefaultDatetime();
   
-  Alarm.alarmRepeat(11, 20, 0, StartMovement);    // Movimentacao do robo comecando as 11:20 todo dia
-  Alarm.alarmRepeat(11, 21, 0, StartStandby);     // Robo ficando em standby a partir das 11:21 todo dia
+  // Robot movements starting every day at minimum time specified
+  Alarm.alarmRepeat(MIN_HOUR_LIMIT, MIN_MINUTE_LIMIT, MIN_SECOND_LIMIT, StartMovement);
+
+  // Robot in standby after the maximum time specified
+  Alarm.alarmRepeat(MAX_HOUR_LIMIT, MAX_MINUTE_LIMIT, MAX_SECOND_LIMIT + 1, StartStandby);
+ 
   //Alarm.alarmRepeat(17,45,0,EveningAlarm);  // 5:45pm every day
   //Alarm.alarmRepeat(dowSaturday,8,30,30,WeeklyAlarm);  // 8:30:30 every Saturday 
 
@@ -20,12 +35,27 @@ void  loop(){
   Alarm.delay(1000); // wait one second between clock display
 }
 
+// setDefaultDatetime(): Specifies default datetime while uploading
+// TODO: Get datetime from computer
+void setDefaultDatetime() {
+  setTime(11, 19, 0, 3, 13, 16);                  // 11:19:00am March 13 2016
+}
+
 void StartMovement(){
-  Serial.println("Inicio de movimentacao do robo");    
+  while(isMovementPeriod()) {
+    Serial.println("Moving");
+    delay(1000);
+  }
+}
+
+// isMovementPeriod(): Checking if the time range is for robot movement or not
+bool isMovementPeriod() {
+  float currentHour = hour() + (minute() / 60.0) + (second() / 3600.0);
+  return currentHour >= minHour && currentHour <= maxHour;
 }
 
 void StartStandby(){
-  Serial.println("Robo em standby - Apenas recebendo energia solar");           
+  Serial.println("Standby - Receiving solar energy");           
 }
 
 void digitalClockDisplay()
